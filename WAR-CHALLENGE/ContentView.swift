a77b067603fb462e62eb3483535131b297854b1f
Mathias DORR <No>
2021-07-15 01:39:15 +0200

    Initial Commit

---
+++ WAR-CHALLENGE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WAR-CHALLENGE
+//
+//  Created by Mathias DORR on 15/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WAR-CHALLENGE/WAR_CHALLENGEApp.swift
@@ -0,0 +1,17 @@
+//
+//  WAR_CHALLENGEApp.swift
+//  WAR-CHALLENGE
+//
+//  Created by Mathias DORR on 15/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct WAR_CHALLENGEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
